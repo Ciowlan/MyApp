6254a037277a53a583513d36ee40b0f62f365ca0
Ann <joanna071878@gmail.com>
2022-01-25 11:44:57 +0800

    加ListView

--- app/src/main/java/tw/com/ian/myapp/MainActivity.java
+++ app/src/main/java/tw/com/ian/myapp/MainActivity.java
@@ -3,9 +3,17 @@ package tw.com.ian.myapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -15,6 +23,13 @@ public class MainActivity extends AppCompatActivity {
     Timer timer;
     private  int intHSec;
     TextView tv;
+    ListView lv;
+    SimpleAdapter adapter;
+    boolean isStart;
+    List<HashMap<String, String>> data;
+    String[] from = {"lap"};
+    int [] to ={R.id.tv_time};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,10 +37,38 @@ public class MainActivity extends AppCompatActivity {
         bt1 = findViewById(R.id.bt1);
         bt2 = findViewById(R.id.bt2);
         tv = findViewById(R.id.tv);
-        timer = new Timer();
-        timer.schedule(new MyTask(),0,10);
+        lv = findViewById(R.id.lv);
+        data = new ArrayList<>();
+        isStart = false;
         intHSec = 0;
     }
+
+    public void lap(View view) {
+
+
+
+        HashMap<String, String> h = new HashMap<>();
+        h.put(from[0],toClock());
+        data.add(h);
+        Log.v("MainActivity",data.toString());
+        adapter = new SimpleAdapter(this,data,R.layout.items,from,to);
+        lv.setAdapter(adapter);
+    }
+
+    public void start(View view) {
+        if (isStart==false){
+            timer = new Timer();
+            timer.schedule(new MyTask(),0,10);
+            bt2.setText("STOP");
+        }else{
+            bt2.setText("START");
+            timer.purge();
+            timer.cancel();
+        }
+
+        isStart = !isStart;
+    }
+
     class MyTask extends TimerTask {
 
         @Override
